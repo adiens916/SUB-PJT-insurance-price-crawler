ec2f0cf164536accd5d65f07927b05b62a12c7f8
Inho <adiens916@naver.com>
2022-03-17 05:43:33 +0900

    feat: Add sample

---
+++ pages/lina/elements.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+BOOK_DOWNLOAD_FOLDER = f'{Path(__file__).parent.parent.parent}/downloads'
+BOOK_LINK_PREFIX = 'https://edu.ssafy.com/data/upload_files/crossUpload/openLrn/ebook/unzip'
---
+++ pages/lina/locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+# class BookPageLocators:
+PAGE_NUMBERS = (By.CLASS_NAME, 'label-page')
+PAGE_IMG = (By.XPATH, '//img[@class="background"]')
